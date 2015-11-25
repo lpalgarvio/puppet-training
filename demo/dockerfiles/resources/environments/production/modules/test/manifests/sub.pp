35941759f6f2914b05e40328b18c159c4deb7ce9
Luís Pedro Algarvio <lp.algarvio@gmail.com>
2015-11-25 00:15:56 +0000

    Updated test module

--- demo/dockerfiles/resources/environments/production/modules/test/manifests/sub.pp
+++ demo/dockerfiles/resources/environments/production/modules/test/manifests/sub.pp
@@ -11,5 +11,13 @@ class test::sub (
 
 ) inherits test {
 
+  $merged_hsh = merge($defaults_hsh, $sub_hsh)
+
+  file { 'test-yaml':
+    ensure => 'present',
+    path   => '/tmp/test.yaml',
+    content => "$merged_hsh",
+  }
+
 }
 
